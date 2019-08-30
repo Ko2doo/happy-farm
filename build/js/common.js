fe42c827c8b0078ec3980e7e6be534666e22307d
NИ <Ko2dooDev@gmail.com>
2019-08-30 14:11:12 +0500

    чистый проект

--- build/js/common.js
+++ build/js/common.js
@@ -1,33 +1,15 @@
-$(document).ready(function(){
-  //Elastic Search
-  $('#search').click(function(elasticSearch){
-    elasticSearch.preventDefault();
-      $('#search').removeClass('close').addClass('active');
-      $('.btn-close').css({"display" : "flex"});
-  });
-  
-  $('.btn-close').click(function(elasticSearch){
-    $('.btn-close').css({"display" : "none"});
-    $('#search.active').removeClass('active').addClass('close');
-  });
-  
+jQuery(document).ready(function($) {
 
-  // табы
-  $(function() {
-    $('ul.info__box-nav').on('click', 'li:not(.active)', function() {
-      $(this)
-        .addClass('active').siblings().removeClass('active')
-        .closest('div.info__box').find('div.info__box-content').removeClass('active').eq($(this).index()).addClass('active');
-    });
-  });
+AOS.init();
 
+	AOS.init({
+	  // Настройки, которые могут быть переопределены для каждого элемента с помощью атрибутов data-aos- *:
+	  delay: 0, // значения от 0 до 3000 с шагом 50 мс
+	  duration: 400, // значения от 0 до 3000 с шагом 50 мс
+	  easing: 'ease', // ослабление по умолчанию для анимации AOS
+	  once: false, // должна ли анимация происходить только один раз - при прокрутке вниз
+	  mirror: true, // должны ли элементы анимироваться при прокрутке мимо них
+	  anchorPlacement: 'top-bottom', // определяет, какая позиция элемента относительно окна должна вызывать анимацию
 
-  $(document).ready(function(){
-  $('a').on('click', function(e){
-    e.preventDefault();
-  });
-
+	});
 });
-  
-});//end
-
